116a3c92769f6bfbc2f72af54182d28c840c1476
Elijah Prince <elijahprince73@gmail.com>
2019-03-05 18:36:37 -0800

    multiple image uploads

--- routes/admin-routes/images.js
+++ routes/admin-routes/images.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+const multer = require('multer');
 const postImageToBucket = require('../../utils/post-image-to-bucket');
+const postManyImagesToBucket = require('../../utils/post-many-images-to-bucket');
 const deleteImageFromBucket = require('../../utils/delete-image-from-bucket');
 const authenticate = require('../../middleware/auth');
 
@@ -7,6 +9,7 @@ const Image = mongoose.model('Image');
 require('dotenv').config({ path: './.env.default' });
 
 module.exports = (app) => {
+  const multipleUpload = multer().array('file');
   app.post('/api/upload', authenticate, (req, res) => {
     const { alt, _id } = req.body;
 
@@ -24,6 +27,26 @@ module.exports = (app) => {
       })
       .catch(() => {
         res.status(400).send('Error saving image to DB');
+      });
+  });
+
+  app.post('/api/upload-many', multipleUpload, authenticate, (req, res) => {
+    const { alt, _id } = req.body;
+
+    postManyImagesToBucket(req, res)
+      .then((imageName) => {
+        const image = new Image({
+          url: `${process.env.SPACES_URL}/${imageName}`,
+          alt,
+          userId: _id,
+        });
+        return image.save();
+      })
+      .then(() => {
+        res.send('Image uploads success');
+      })
+      .catch(() => {
+        res.status(400).send('Error saving images to DB');
       });
   });
 
---
+++ utils/post-many-images-to-bucket.js
@@ -0,0 +1,35 @@
+/* eslint-disable */
+const AWS = require('aws-sdk');
+const multer = require('multer');
+require('dotenv').config({ path: './.env.default' });
+
+module.exports = (req, res) => {
+    const file = req.files;
+    const spacesEndpoint = new AWS.Endpoint(process.env.SPACES_NAME);
+    let s3bucket = new AWS.S3({
+        endpoint: spacesEndpoint,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_key,
+        Bucket: process.env.BUCKET_NAME
+    });
+    return new Promise((resolve, reject) => {
+        s3bucket.createBucket(function () {
+            var ResponseData = [];
+
+            file.map((file) => {
+                var params = {
+                    Bucket: process.env.BUCKET_NAME,
+                    Key: file.originalname,
+                    Body: file.buffer,
+                    ACL: 'public-read'
+                };
+                s3bucket.upload(params, function (err, data) {
+                    if (err) {
+                        reject(err)
+                    } 
+                    resolve()
+                });
+            });
+        });
+    })
+};
