cb219327dcb3505994df9cac5eb987974aa13268
Elijah Prince <elijahprince73@gmail.com>
2019-03-23 23:33:52 -0700

    Small doc changes

--- routes/admin-routes/auth.js
+++ routes/admin-routes/auth.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const authenticate = require('../../middleware/auth');
 
 const User = mongoose.model('User');
 
 module.exports = (app) => {
-  // ADMIN Auth Routes
+  // Admin register
   app.post('/api/admin-register', (req, res) => {
     if (req.body.password !== req.body.passwordConf) {
       res.status(403).send({
@@ -25,7 +26,7 @@ module.exports = (app) => {
         res.status(400).send(err);
       });
   });
-
+  // Admin login
   app.post('/api/admin-login', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -39,5 +40,9 @@ module.exports = (app) => {
           errorMessage: 'Invalid Login',
         });
       });
+  });
+
+  app.get('/api/me', authenticate, (req, res) => {
+    res.send(req.user);
   });
 };
