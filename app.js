626484c7e12bcb27635a0a95e385284646084731
Elijah Prince <elijahprince@Elijahs-MacBook-Pro.local>
2020-02-23 14:36:23 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+const routes = require('./routes')
+
+app.use('/', routes)
+
+module.exports = app;
